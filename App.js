1f9d24f4cd9924b493aa1786aeed8bef9747fedc
mehmethanedar <mehmethanedar@hotmail.com>
2020-12-02 20:22:48 +0300

    hata düzeltimi

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-
-import AppContainer from './routes';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <AppContainer />;
+  return (
+    <View style={styles.container}>
+      <Text>Merhaba</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
